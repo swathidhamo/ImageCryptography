6031c4255baff191e87e9c76766c48cfc35cfa1f
Swathi Dhamodaran <swathidhamo@gmail.com>
2018-04-10 17:33:35 +0530

    Add files via upload

---
+++ Convert.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.ComponentModel; 
+using System.Threading.Tasks;
+
+namespace RSA_Algorithm
+{
+    class Convert
+    {
+        static public byte[] Decryption(byte[] Data, RSAParameters RSAKey, bool DoOAEPPadding)
+        {
+            try
+            {
+                byte[] decryptedData;
+                using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
+                {
+                    RSA.ImportParameters(RSAKey);
+                    decryptedData = RSA.Decrypt(Data, DoOAEPPadding);
+                }
+                Console.WriteLine(decryptedData); 
+                return decryptedData;
+            }
+            catch (CryptographicException e)
+            {
+                Console.WriteLine(e.ToString());
+                return null;
+            }
+        }
+        static public byte[] Encryption(byte[] Data, RSAParameters RSAKey, bool DoOAEPPadding)
+        {
+            try
+            {
+                byte[] encryptedData;
+                using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
+                {
+                    RSA.ImportParameters(RSAKey); encryptedData = RSA.Encrypt(Data, DoOAEPPadding);
+                }
+           //     Console.WriteLine(ByteConverter.getBytes(encryptedData));
+                Console.WriteLine("End of the function"); 
+                return encryptedData;
+            }
+            catch (CryptographicException e)
+            {
+                Console.WriteLine(e.Message);
+                return null;
+            }
+        }
+    }
+}
--- Form1.cs
+++ Form1.cs
@@ -3,64 +3,83 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace ImageProcessingInit
+namespace RSA_Algorithm
 {
     public partial class Form1 : Form
     {
+
+        UnicodeEncoding ByteConverter = new UnicodeEncoding();
+        RSACryptoServiceProvider RSA = new RSACryptoServiceProvider();
+        byte[] plaintext;
+        byte[] encryptedtext;
+
+
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-
-            Bitmap copy = new Bitmap((Bitmap)this.originalImage.Image);
-            Processing.ConvertToGrayScale(copy);
-            this.grayImage.Image = copy;
-
-        }
-
         private void button1_Click(object sender, EventArgs e)
         {
-            OpenFileDialog ofile = new OpenFileDialog();
-            ofile.Filter = "Image file (*.bmp, *.jpg)|*.bmp;*.jpg";
-            if (DialogResult.OK == ofile.ShowDialog())
-            {
-                this.originalImage.Image = new Bitmap(ofile.FileName);
-            }
 
+            String line;
+            FileInfo MyFile = new FileInfo(@"C:\Users\lenovo\source\repos\ImageProcessingInit\ImageProcessingInit\bin\Debug\gray.txt");
+            //Instantiate a StreamReader to read from the text file.
+            StreamReader sr = MyFile.OpenText();
+            //Read a single character.
+            string fileInput = sr.ReadToEnd();
+            plaintext = ByteConverter.GetBytes("#F324DF");
+
+            //Console.WriteLine(ByteConverter.GetString(plaintext)); 
+            encryptedtext = Convert.Encryption(plaintext, RSA.ExportParameters(false), false);
+            StreamWriter sw = new StreamWriter(@"C: \Users\lenovo\writefile1.txt");
+            Console.WriteLine(ByteConverter.GetString(encryptedtext));
+
+            sw.Close();
+            sr.Close();
 
         }
+        public static byte[] ConvertInt32ToByteArray(Int32 I32)
 
-        private void pictureBox2_Click(object sender, EventArgs e)
         {
 
-        }
+            return BitConverter.GetBytes(I32);
 
-        private void button3_Click(object sender, EventArgs e)
+        }
+        private void button2_Click(object sender, EventArgs e)
         {
-            Bitmap copy = new Bitmap((Bitmap)this.originalImage.Image);
-            if (Processing.ConvertImageToHex(copy))
-            {
-                Console.WriteLine("It happened"); 
-            }
-        
+            //  StreamReader sr = new StreamReader(@"C:\Users\lenovo\writefile.txt");
+            String line;
+            //line = sr.ReadLine();
+
+            byte[] decryptedtex = Convert.Decryption(encryptedtext, RSA.ExportParameters(true), false);
+            Console.WriteLine(ByteConverter.GetString(decryptedtex));
+            Console.WriteLine("HI thre");
+            //while (line != null)
+            //{
+            //decy = ByteConverter.GetBytes(line);
+            //decryptedtex = Convert.Decryption(decy, RSA.ExportParameters(true), false);
+            //line = sr.ReadLine();
+            //decy = ByteConverter.GetBytes(line); 
+
+            //}
+            //decryptedtex = Convert.Decryption(test, RSA.ExportParameters(true), false);
+            //Console.WriteLine(decryptedtex); 
+            //  StreamWriter sw = new StreamWriter(@"C:\Users\lenovo\images1.txt");
+
+            //  sw.WriteLine(decryptedtex);
+            //  sr.Close();
+
         }
 
-        private void originalImage_Click(object sender, EventArgs e)
-        {
 
-        }
     }
+
 }
